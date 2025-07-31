33639b4dffd6028b33f8a52c8653d42457fe4b0f
Charles Mesa Cayobit <charlesmesacayobit@gmail.com>
2025-07-31 16:15:09 -0700

    Simplify package structure

--- organize_directory_contents/__init__.py
+++ organize_directory_contents/__init__.py
@@ -6,6 +6,24 @@ from pathlib import Path
 MISC_DIR = "Misc"
 
 
+def move_file(file: Path, target_dir: Path) -> None:
+    """Move `file` into `target_dir`."""
+
+    file.rename(target_dir / file.name)
+
+
+def move_image(image_file: Path, target_dir: Path) -> None:
+    """Move an image and its sidecar file to `target_dir`."""
+
+    move_file(image_file, target_dir)
+
+    sidecar_file = image_file.with_suffix(".xmp")
+    try:
+        move_file(sidecar_file, target_dir)
+    except FileNotFoundError:
+        pass  # Do nothing if a sidecar file does not exist.
+
+
 def read_from_file(file: Path) -> tuple[set[str], dict[str, str]]:
     """Create a targets dictionary from a file."""
 
--- organize_directory_contents/__main__.py
+++ organize_directory_contents/__main__.py
@@ -6,7 +6,51 @@ __author__ = "Charles Mesa Cayobit"
 from argparse import ArgumentParser
 from pathlib import Path
 
-from organize_directory_contents.main import main
+import organize_directory_contents as odc
+
+
+def main(root_dir: Path, config_file: Path) -> None:
+    subdirs, target_dirs = odc.read_from_file(config_file)
+
+    for subdir in subdirs:
+        (root_dir / subdir).mkdir(parents=True, exist_ok=True)
+
+    # `move_image()` will move an image's existing sidecar file alongside the
+    # image, so defer processing XMP files to the end.
+    xmp_files: list[Path] = []
+
+    images_dir = target_dirs["jpg"]
+    images_raw_dir = target_dirs["dng"]
+    for file in root_dir.iterdir():
+        if file.name in subdirs or file.name == ".DS_Store":
+            continue
+
+        if file.is_dir():
+            odc.move_file(file, root_dir / odc.MISC_DIR)
+
+        file_ext = file.suffix
+        if not file_ext:
+            odc.move_file(file, root_dir / odc.MISC_DIR)
+            continue
+
+        file_ext = file_ext[1:].lower()
+        if file_ext == "xmp":
+            xmp_files.append(file)
+            continue
+
+        target_dir = target_dirs.get(file_ext, odc.MISC_DIR)
+        if target_dir == images_dir or target_dir == images_raw_dir:
+            odc.move_image(file, root_dir / target_dir)
+
+        else:
+            odc.move_file(file, root_dir / target_dir)
+
+    for xmp_file in xmp_files:
+        try:
+            odc.move_file(xmp_file, root_dir / odc.MISC_DIR)
+        except FileNotFoundError:
+            pass  # Do nothing if the image sidecar file had already been moved.
+
 
 if __name__ == "__main__":
     parser = ArgumentParser(prog="Organize Directory", description=__doc__)
